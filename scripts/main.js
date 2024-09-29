3e5e0c7a1dbc160f9a6cbb382530dafa4b41c0e5
notaslakhan <aslakhanov432@gmail.com>
2024-09-29 19:06:27 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  effect: "cards",
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
